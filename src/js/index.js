60289a5830b4d191296faf23cae477c227796914
Ankit Patel <ankitpansuriya007@gmail.com>
2021-07-09 14:33:27 +0530

    babel runtime support

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,50 @@
 import '../scss/styles.scss';
 
 document.querySelector('button').addEventListener('click', () => alert('test'));
+
+let numbers = [1,2,3,4,5];
+
+let sum = 0;
+let result = numbers.reduce((number, sum) => number + sum);
+
+console.log('sum', sum);
+console.log(result);
+
+class Person {
+    constructor() {
+        this.printPersons();
+    }
+
+    getPersonsData() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (false) {
+                    let persons = [{
+                        name: 'Ankit'
+                    }, {
+                        name: 'Akshay'
+                    }];
+    
+                    resolve(persons);
+                } else {
+                    reject(new Error('data not found'));
+                }
+            },5000);
+        });
+    }
+
+    async printPersons() {
+        let persons = await this.getPersonsData();
+        console.log(persons);
+
+        // this.getPersonsData().then(data => {
+        //     data.forEach(element => {
+        //         console.log(element.name);
+        //     });
+        // }).catch(err => {
+        //     console.log(err);
+        // });
+    }
+}
+
+new Person();
